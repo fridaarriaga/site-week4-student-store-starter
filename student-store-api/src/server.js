fd8a0875f8af48cb0dcc99db6cfb0a2866fe9217
Frida Arriaga <farriaga@farriaga-ltmrdwk.internal.salesforce.com>
2026-06-21 14:44:56 -0700

    RAHHH

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const prisma = require("./db/db")
 const Product = require("../models/product")
 const Order = require("../models/order")
@@ -22,6 +23,7 @@ const formatOrderWithItems = (order) => {
 }
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" })
